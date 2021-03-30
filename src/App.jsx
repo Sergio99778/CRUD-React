7278bd9a19e7ad99a8b9021a9ee94f3cc10e0877
sergio99778 <sergio4brandon@gmail.com>
2021-03-29 21:44:53 -0600

    Adding basic installation.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+    return (
+        <div className="container">
+            <p className="btn btn-success"></p>
+        </div>
+    )
+}
+export default App
